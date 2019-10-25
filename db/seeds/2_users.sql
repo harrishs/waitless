d22fc78d8dea2e70519225e2ff9d5c5074e8d917
Kevin Convery <kevinconvery@gmail.com>
2019-10-25 13:07:57 -0400

    add user dummy data

--- db/seeds/2_users.sql
+++ db/seeds/2_users.sql
@@ -1,14 +1,35 @@
   -- bcrypt hashed version of the word 'password' for all users
   '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
 
-INSERT INTO users (id, name, email, password, phone_number, owns_restaurant)
+INSERT INTO users (name, email, password, phone_number)
 VALUES
 (
-  'AAAAAA',
   'Bob Jones',
-  'bobjones@bob.com',
+  'bob@jones.com',
   -- bcrypt hashed version of the word 'password' for all users
   '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
-  6476378535,
-  FALSE
+  4164112034
 ),
+(
+  'Sam Spade',
+  'sam@spade.com',
+  -- bcrypt hashed version of the word 'password' for all users
+  '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+  4165445667
+),
+(
+  'Barry Red',
+  'barry@red.com',
+  -- bcrypt hashed version of the word 'password' for all users
+  '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+  4164112034
+),
+-- DROP TABLE IF EXISTS users CASCADE;
+
+-- CREATE TABLE users (
+--   id SERIAL PRIMARY KEY,
+--   name VARCHAR(255) NOT NULL,
+--   email VARCHAR(255) NOT NULL,
+--   password VARCHAR(255) NOT NULL,
+--   phone_number VARCHAR(10) NOT NULL
+-- );
