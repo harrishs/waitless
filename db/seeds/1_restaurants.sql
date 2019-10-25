7febf3dff9fba740aef0a017bd683f081ad0c03f
Kevin Convery <kevinconvery@gmail.com>
2019-10-25 15:07:45 -0400

    resolve conflict

--- db/seeds/1_restaurants.sql
+++ db/seeds/1_restaurants.sql
@@ -1,18 +1,6 @@
 -- all data's fake right now
 -- just testing connection to db
 
-<<<<<<< HEAD
-INSERT INTO restaurants (name, type, street_number, street_name, postal_code, phone_number, active, email, password, image_url)
-VALUES
-(
-  'Soufi',
-  'Middle Eastern',
-  '444',
-  'Semantic Lane',
-  'M0M 0M0'
-  '4164444444',
-  TRUE,
-=======
 INSERT INTO restaurants (name, type, street_number, street_name, postal_code, phone_number,  email, password, image_url)
 VALUES
 (
@@ -22,22 +10,11 @@ VALUES
   'Semantic Lane',
   'M0M 0M0',
   '4164444444',
->>>>>>> db/seeds
   'jimmy@soufis.com',
   '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
   ''
 ),
 (
-<<<<<<< HEAD
-  "Striped Pizza",
-  "Italian",
-  "52",
-  "Weekly Road",
-  "4162224422",
-  "M2E 1Z6",
-  TRUE,
-  "gino@stripedpizza.net",
-=======
   'Striped Pizza',
   'Italian',
   '52',
@@ -45,22 +22,11 @@ VALUES
   'M2E 1Z6',
   '4162224422',
   'gino@stripedpizza.net',
->>>>>>> db/seeds
   -- bcrypt for 'password'
   '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
   ''
 ),
 (
-<<<<<<< HEAD
-  "Lemongrass Thai Cuisine",
-  "Thai",
-  "125",
-  "Salad Lane",
-  "9053254475",
-  "M2Z 2R7",
-  TRUE,
-  "manager@lemongrass.com",
-=======
   'Lemongrass Thai Cuisine',
   'Thai',
   '125',
@@ -68,7 +34,6 @@ VALUES
   'M2Z 2R7',
   '9053254475',
   'manager@lemongrass.com',
->>>>>>> db/seeds
     -- bcrypt for 'password'
   '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
   ''
