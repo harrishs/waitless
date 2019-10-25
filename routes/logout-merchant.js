5c7deb93f6e217c0fb70cc3e4459308b576468fc
Corah Goyette <corahgoyette@gmail.com>
2019-10-25 14:43:59 +0000

    routes

--- routes/logout-merchant.js
+++ routes/logout-merchant.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const router  = express.Router();
+
+
+router.get("/", (req, res) => {
+  res.redirect("/");
+})
+
+
+module.exports =  router
+
--- routes/logout-user.js
+++ routes/logout-user.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const router  = express.Router();
+
+
+router.get("/", (req, res) => {
+  res.redirect("/");
+})
+
+module.exports =  router
+
