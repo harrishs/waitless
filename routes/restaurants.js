5377e5f37e2ae3e42ba774a8e6563124e032a92d
Kevin Convery <kevinconvery@gmail.com>
2019-10-25 23:31:51 -0400

    add get /restaurants/id route

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router  = express.Router();
 
+// /restaurants routes
 module.exports = (db) => {
   let data = {};
+
+  // GET - /restaurants
+  // Queries all restaurants and renders them.
   router.get("/", (req, res) => {
     const queryString = "SELECT * FROM restaurants";
     db.query(queryString)
+      .then((resultSet) => {
+        data.restaurants = resultSet.rows;
+        res.render('user-restaurants', data);
+      })
+      .catch(err => console.log(err));
+  });
+
+  // GET /:id => /restaurants/:id
+  // Gets a specific restaurant from the database where the id matches the route.
+  router.get("/:id", (req, res) => {
+    const queryString = `SELECT * FROM restaurants WHERE restaurants.id = $1`;
+    db.query(queryString, [req.params.id])
       .then((resultSet) => {
         data.restaurants = resultSet.rows;
         res.render('user-restaurants', data);
