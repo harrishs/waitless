a48ce57111b56d297ac477c50b69ddf72be5531a
Kevin Convery <kevinconvery@gmail.com>
2019-10-26 19:16:19 -0400

    work create route

--- routes/waitlists.js
+++ routes/waitlists.js
@@ -1,16 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-// GET /new
-// CREATE route
-
 module.exports = (db) => {
-
-  // GET /new
-  router.get("/new", (req, res) => {
-    res.send("Hit waitlist create route!");
-  });
-
   // POST /:id => /waitlists/:id
   // Adds the user into the restaurant's waitlist.
   router.post("/:id", (req, res) => {
@@ -18,13 +9,13 @@ module.exports = (db) => {
     // the user into the database but doing very basic insert for now
     const insertString =
       `INSERT INTO waitlist_entries (waitlist_id, user_id, booked_at, party_size) VALUES
-       ($1, 1, $2, 2)
+       ($1, 1, $2, $3)
       `;
-    const insertParameters = [req.params.id, Date.now()];
+    const insertParameters = [req.params.id, Date.now(), req.body.party_size];
     db.query(insertString, insertParameters)
       .then(() => {
         // insert, redirect
-        console.log(`Successfully added user for waitlist ${req.params.id}!`)
+        // console.log(`Successfully added user for waitlist ${req.params.id}! Party size of ${req.body.party_size}!`)
         res.redirect('/restaurants');
       })
       .catch(err => console.error(err));
