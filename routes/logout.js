b968830cb413e8ffec0b0da0070d0eae203cb0fe
Kevin Convery <kevinconvery@gmail.com>
2019-10-27 19:15:11 -0400

    modify logout route

--- routes/logout.js
+++ routes/logout.js
@@ -2,15 +2,11 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  router.get("/users", (req, res) => {
+  router.post("/", (req, res) => {
     req.session.destroy(() => {
       res.redirect('/');
     });
-    // res.render("logout-user")
   });
 
-  router.get("/merchants", (req, res) => {
-    res.render("logout-merchant");
-  });
   return router;
 };
