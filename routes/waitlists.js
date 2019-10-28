0189370b6bee04ad1ece3d6b89b907bf5cd2ae16
Kevin Convery <kevinconvery@gmail.com>
2019-10-28 09:56:13 -0400

    working on db query for updating user record

--- routes/waitlists.js
+++ routes/waitlists.js
@@ -7,13 +7,23 @@ module.exports = (db) => {
   router.post("/:id", (req, res) => {
     req.session.waitlistId = req.params.id;
     const insertString =
-      `INSERT INTO waitlist_entries (waitlist_id, user_id, booked_at, party_size) VALUES
+      `INSERT INTO waitlist_entries (waitlist_id, booked_at, party_size, party_name) VALUES
        ($1, $2, $3, $4)
+       RETURNING id
       `;
-    const insertParameters = [req.params.id, 1, Date.now(), req.body.party_size];
+    const insertParameters = [req.params.id, Date.now(), req.body.party_size, 'John'];
+    const updateString =
+      `UPDATE users
+       SET booking_id = $1
+       WHERE id = $2;
+      `;
+    const updateParameters = "";
     db.query(insertString, insertParameters)
-    .then(() => {
-      res.redirect("/restaurants");
+    .then((resultSet) => {
+      // this is the id returned in the
+      console.log(resultSet.rows[0].id);
+      res.send(`New waitlist entry with id ${id} added!`);
+      // res.redirect("/restaurants");
     })
     .catch(err => console.error(err));
   });
