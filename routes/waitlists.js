1f10c100654856de46e931fbd11c1513cb3d962f
Kevin Convery <kevinconvery@gmail.com>
2019-10-28 10:25:21 -0400

    working on delete functionality for waitlists

--- routes/waitlists.js
+++ routes/waitlists.js
@@ -24,7 +24,6 @@ module.exports = (db) => {
       // which will now be used in the update of the appropriate user record.
       const booking_id = resultSet.rows[0].id;
       const updateParameters = [booking_id, req.session.user_id];
-      // res.send(`New waitlist entry with id ${booking_id} added!`);
       db.query(updateString, updateParameters)
       .then(() => {
         res.send("Success updating user!");
@@ -34,12 +33,24 @@ module.exports = (db) => {
     .catch(err => console.error(err));
   });
 
+  // DELETE /:id => /waitlists/delete
+  // Removes a user from a given waitlist. The waitlist entry is deleted and the user's booking_id
+  // is set back to null.
   router.delete("/:id", (req, res) => {
     const deleteString =
       `DELETE FROM waitlist_entries WHERE user_id = $1`;
     const deleteParameters = [req.session.user_id];
     db.query(deleteString, deleteParameters)
     .then(() => {
+      const updateString = `
+        UPDATE users
+        SET booking_id = $1
+        WHERE id = $2;
+      `
+      const updateParameters = [null, req.session.user_id];
+      db.query(updateString, updateParameters)
+      .then()
+
       req.session.waitlistId = null;
       res.redirect('/restaurants')
     })
