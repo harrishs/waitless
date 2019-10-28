9b89201892117106f1bbedbfc5d48bdadb4a41f0
Kevin Convery <kevinconvery@gmail.com>
2019-10-27 20:30:26 -0400

    more cleanup

--- routes/waitlists.js
+++ routes/waitlists.js
@@ -5,14 +5,6 @@ module.exports = (db) => {
   // POST /:id => /waitlists/:id
   // Adds the user into the restaurant's waitlist.
   router.post("/:id", (req, res) => {
-    // gonna have to be tracking the session at some point here to insert
-    // the user into the database but doing very basic insert for now
-    // if (req.session.waitlistId) {
-    //   console.log("Already booked!");
-    //   res.redirect('/');
-    // } else {
-    //   console.log("Thank you for booking!");
-    // }
     req.session.waitlistId = req.params.id;
     const insertString =
       `INSERT INTO waitlist_entries (waitlist_id, user_id, booked_at, party_size) VALUES
@@ -21,8 +13,6 @@ module.exports = (db) => {
     const insertParameters = [req.params.id, 1, Date.now(), req.body.party_size];
     db.query(insertString, insertParameters)
     .then(() => {
-      // insert, redirect
-      // console.log(`Successfully added user for waitlist ${req.params.id}! Party size of ${req.body.party_size}!`)
       res.redirect("/restaurants");
     })
     .catch(err => console.error(err));
