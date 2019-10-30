f37398d3244514173ea0805478bccd3a4e8f03da
Kevin Convery <kevinconvery@gmail.com>
2019-10-29 22:41:42 -0400

    add new map seeds

--- bin/resetmapdb.sql
+++ bin/resetmapdb.sql
@@ -3,6 +3,6 @@
 \i db/schema/3_waitlists.sql
 \i db/schema/4_waitlist_entries.sql
 \i db/schema/5_permissions.sql
-\i db/seeds/mapseeds/1_restaurants.sql
-\i db/seeds/mapseeds/2_users.sql
-\i db/seeds/mapseeds/3_waitlists.sql
+\i db/seeds/map-seeds/1_restaurants.sql
+\i db/seeds/map-seeds/2_users.sql
+\i db/seeds/map-seeds/3_waitlists.sql
