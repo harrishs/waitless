51fb4e5c565f80c4ec8e349c24c43969ddd90238
Kevin Convery <kevinconvery@gmail.com>
2019-11-03 22:59:25 -0500

    write login code to async/await - prepare to write error handling

--- routes/login.js
+++ routes/login.js
@@ -3,132 +3,91 @@ const router  = express.Router();
 const bcrypt = require('bcrypt');
 
 module.exports = (db) => {
+  let data = {
+    user: '',
+    error: {
+      registerError: false,
+      loginError: false,
+      details: ''
+    },
+    email: ''
+  };
 
   // Route to show the login page for users
   router.get("/users", (req, res) => {
-    let data = {
-      user: '',
-      error: {
-        registerError: false,
-        loginError: false,
-        details: ''
-      },
-      email: ''
-    };
     res.render("login-user", data);
   });
 
   // Route to login registered users
-  router.post("/users", (req,res) => {
-    let data = {
-      user: '',
-      error: {
-        registerError: false,
-        loginError: false,
-        details: ''
-      },
-      email: ''
-    };
+  router.post("/users", async (req,res) => {
+    try {
+      let email = req.body.email.trim().toLowerCase();
+      let password =  req.body.password.trim();
 
-    let email = req.body.email.trim().toLowerCase();
-    let password =   req.body.password.trim();
+      const userQuery = `
+        SELECT * FROM users
+        WHERE users.email = $1
+        LIMIT 1
+      `;
 
-    let emptyField = email.length === 0 || password.length === 0 ? true : false;
+      const userInfo = await db.query(userQuery, [email]);
+      const response = userInfo.rows[0];
 
-      if (emptyField) {
-        res.status(400).send('Please specify email and password');
+      if (response !== undefined && bcrypt.compareSync(password, response.password)) {
+        req.session.user_id = response.id;
+        req.session.email = response.email;
+        req.session.name = response.name;
+        req.session.phone_number = response.phone_number;
+        req.session.bookingId = response.booking_id;
+        data.user = response.name;
+        data.email = response.email;
+        data.error.loginError = false;
+        res.redirect('/restaurants');
       } else {
-        const userQuery = `SELECT * FROM users
-                           WHERE users.email = $1
-                           LIMIT 1
-                          `;
-
-        db
-          .query(userQuery, [email])
-          .then(userInfo => {
-          let response = userInfo.rows[0];
-            if (response !== undefined && bcrypt.compareSync(password, response.password)) {
-              req.session.user_id = response.id;
-              req.session.email = response.email;
-              req.session.name = response.name;
-              req.session.phone_number = response.phone_number;
-              req.session.bookingId = response.booking_id;
-              data.user = response.name;
-              data.email = response.email;
-              data.error.loginError = false;
-              res.redirect('/restaurants');
-            } else {
-              data.error.loginError = true;
-              res.status(400).send("Username or password is incorrect");
-            }
-          })
-          .catch(err => {
-            data.error.loginError = true;
-            res.status(400).send(err.message);
-          });
+        data.error.loginError = true;
+        res.status(400).send("Username or password is incorrect");
       }
+    } catch(err) {
+      console.log(err);
+    }
   })
 
   // Route to show login page for merchants
   router.get("/merchants", (req, res) => {
-    let data = {
-      user: '',
-      error: {
-        registerError: false,
-        loginError: false,
-        details: ''
-      },
-      email: ''
-    };
     res.render("login-merchant", data);
   });
 
   // Route to login merchanges
-  router.post("/merchants", (req,res) => {
-    let data = {
-      user: '',
-      error: {
-        registerError: false,
-        loginError: false,
-        details : ''
-      },
-      email: ''
-    };
+  router.post("/merchants", async (req,res) => {
+    try {
+      let email = req.body.email.trim().toLowerCase();
+      let password = req.body.password.trim();
 
-    let email = req.body.email.trim().toLowerCase();
-    let password = req.body.password.trim();
+      const userQuery = `
+        SELECT * FROM restaurants
+        WHERE restaurants.email = $1
+        LIMIT 1
+      `;
 
-    let emptyField = email.length === 0 || password.length === 0 ? true : false;
-    if (emptyField) {
-      res.status(400).send('Please make sure all fields are filled out');
-    } else {
-      const userQuery = `SELECT * FROM restaurants
-                         WHERE restaurants.email = $1
-                         LIMIT 1 `;
+      const userInfo = await db.query(userQuery, [email]);
+      const response = userInfo.rows[0];
 
-      db
-        .query(userQuery, [email])
-        .then(userInfo => {
-          let response = userInfo.rows[0];
-          if (response !== undefined && bcrypt.compareSync(password, response.password)) {
-              req.session.user_id = response.id;
-              req.session.email = response.email;
-              req.session.name = response.name;
-              data.user = response.name;
-              data.email = response.email;
-              data.error.loginError = false;
-              res.redirect("/waitlist")
-          } else {
-            data.error.loginError = true;
-            res.status(400).send("Username or password is incorrect");
-            // res.render('login-merchant', data);
-          }
-        })
-        .catch(err => {
-          data.error.loginError = true;
-          res.status(400).send(err.message);
-          // res.render('login-merchant', data);
-        });
+      if (response !== undefined && bcrypt.compareSync(password, response.password)) {
+        req.session.user_id = response.id;
+        req.session.email = response.email;
+        req.session.name = response.name;
+        data.user = response.name;
+        data.email = response.email;
+        data.error.loginError = false;
+        res.redirect("/waitlist")
+      } else {
+        data.error.loginError = true;
+        res.status(400).send("Username or password is incorrect");
+        // res.render('login-merchant', data);
+      }
+
+    } catch(err) {
+      console.log(err);
     }
   });
 
