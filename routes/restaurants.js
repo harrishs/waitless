a4f7287bad42915d310c1e48293e7a939e4fb931
Kevin Convery <kevinconvery@gmail.com>
2019-11-04 21:08:13 -0500

    modify sql query in search to display booked restaurant first

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -22,10 +22,12 @@ module.exports = (db) => {
         data.errorMessage = "";
       }
       let queryString = `
-        SELECT restaurants.id, restaurants.name, restaurants.address, restaurants.type, restaurants.image_url, waitlists.id AS waitlist_id, waitlists.wait_time
+        SELECT restaurants.id, restaurants.name, restaurants.address, restaurants.type, restaurants.image_url, waitlists.id AS waitlist_id, waitlists.wait_time, waitlist_entries.id
         FROM restaurants
-        LEFT JOIN waitlists
-        ON restaurants.id=waitlists.restaurant_id
+        LEFT JOIN waitlists ON restaurants.id=waitlists.restaurant_id
+        LEFT JOIN waitlist_entries ON waitlist_entries.waitlist_id=waitlists.id
+        LEFT JOIN users ON waitlist_entries.id=users.booking_id
+        ORDER BY waitlist_entries.id
       `;
       let resultSet = await db.query(queryString);
       data.restaurants = resultSet.rows;
