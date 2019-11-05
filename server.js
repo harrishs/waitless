e1168a563ef82d17df046ff437ee97a19c4dd697
Harrish Suhumar <harrish.s@hotmail.ca>
2019-11-05 18:35:17 +0000

    shows reload msg on all reloads

--- server.js
+++ server.js
@@ -30,10 +30,12 @@ io.on('connection', function (socket) {
   socket.on('reload', function (data) {
     console.log("reload", data);
     socket.broadcast.emit("reload", {...data});
+    socket.disconnect();
   });
   socket.on('load', function (data) {
     console.log("reload", data);
     socket.broadcast.emit("load", {...data});
+    socket.disconnect();
   });
 });
 
