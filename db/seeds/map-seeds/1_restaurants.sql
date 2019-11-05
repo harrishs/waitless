d77648207889628d5276f4ad18a75045272f0a22
Kevin Convery <kevinconvery@gmail.com>
2019-11-05 10:57:33 -0500

    add more restaurants into seed

--- db/seeds/map-seeds/1_restaurants.sql
+++ db/seeds/map-seeds/1_restaurants.sql
@@ -117,7 +117,201 @@ VALUES
   -- bcrypt for 'password'
   '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
   'images/fish.jpg'
+),
+(
+ 'Hy''s Steakhouse',
+ 'Steakhouse',
+ '365 Bay Street',
+ 'M5H 2V1',
+ '4164218120',
+ 'hy@steakhouse.com',
+ '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+ 'images/hy.jpg'
+),
+(
+ 'Kotta Sushi',
+ 'Japanese',
+ '1226 King Street West',
+ 'M6K 1G4',
+ '9053121292',
+ 'kotta@sushi.com',
+ '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+ 'images/kotta.jpg'
+),
+(
+ 'WVRST',
+ 'Family Friendly',
+ '609 King Street West',
+ 'M5V 1M5',
+ '4164421232',
+ 'em@wvrst.com',
+ '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+ 'images/wvrst.jpg'
+),
+(
+ 'Buca',
+ 'Italian',
+ '604 King Street West',
+ 'M5V 1M5',
+ '4166723495',
+ 'buca@italian.com',
+ '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+ 'images/buca.jpg'
+),
+(
+ 'Chubby''s Jamaican Kitchen',
+ 'Jamaican',
+ '104 Portland Street',
+ 'M5V 2N2',
+ '6471291283',
+ 'chubby@kitchen.com',
+ '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+ 'images/chubby.jpg'
+),
+(
+ 'Lee',
+ 'Asian Fusion',
+ '601 King Street West',
+ 'M5V 1M5',
+ '4164444441',
+ 'sam@lee.com',
+ '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+ 'images/lee.jpg'
+),
+(
+ 'Gusto 101',
+ 'Italian',
+ '101 Portland Street',
+ 'M5V 2N3',
+ '4164441262',
+ 'sam@mazeend.com',
+ '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+ 'images/gusto.jpg'
+),
+(
+ 'Thompson Diner',
+ 'American',
+ '51 Bathurst Street',
+ 'M5V 2P3',
+ '4164124444',
+ 'sam@thompson.com',
+ '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+ 'images/thompson.jpg'
+),
+(
+ 'Wilbur Mexicana',
+ 'Mexican',
+ '552 King Street West',
+ 'M5V 1M3',
+ '6474441244',
+ 'sam@mexicana.com',
+ '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+ 'images/wilbur.jpg'
+),
+(
+ 'iQ',
+ 'Healthy',
+ '613 King Street West',
+ 'M5V 1M5',
+ '4164121283',
+ 'sam@iq.com',
+ '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+ 'images/iq.jpg'
+),
+(
+ 'Lawai''a Poke',
+ 'Hawaiian',
+ '700 King Street West',
+ 'M5V 2Y6',
+ '4164234414',
+ 'lawai@poke.com',
+ '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+ 'images/lawai.jpg'
+),
+(
+ 'Coco Rice',
+ 'Thai',
+ '669 King Street West',
+ 'M5V 1M9',
+ '4161261283',
+ 'coco@rice.com',
+ '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+ 'images/coco.jpg'
+),
+(
+ 'Rodney''s Oyster',
+ 'Oyster Bar',
+ '469 King Street West',
+ 'M5V 3M4',
+ '6471239128',
+ 'rodney@oyster.com',
+ '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+ 'images/rodney.jpg'
+),
+(
+ 'Edulis Restaurant',
+ 'Canadian',
+ '169 Niagara Street',
+ 'M5V 1C9',
+ '6471291273',
+ 'edulis@sam.com',
+ '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+ 'images/edulis.jpg'
+),
+(
+ 'Campechano',
+ 'Mexican',
+ '504 Adelaide Street West',
+ 'M5V 1T4',
+ '9054361728',
+ 'bob@mexican.com',
+ '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+ 'images/campechano.jpg'
+),
+(
+ 'Baro',
+ 'Spanish',
+ '485 King Street West',
+ 'M5V 3M4',
+ '4164412894',
+ 'em@baro.com',
+ '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+ 'images/baro.jpg'
+),
+(
+ 'SARA',
+ 'Fine Dining',
+ '98 Portland Street',
+ 'M5V 2N2',
+ '4164541234',
+ 'sara@dining.com',
+ '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+ 'images/sara.jpg'
+),
+(
+'Fat Bastard',
+ 'Mexican',
+ '628 King Street West',
+ 'M5V 1M7',
+ '4164232123',
+ 'sam@bastard.com',
+ '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+ 'images/fat.jpg'
+),
+(
+ 'Mira',
+ 'Peruvian',
+ '420 Wellington Street West',
+ 'M5V 1E3',
+ '4164445644',
+ 'mira@peruvian.com',
+ '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+ 'images/mira.jpg'
 );
+
+
+
+
 -- CREATE TABLE restaurants (
 --   id SERIAL PRIMARY KEY,
 --   name VARCHAR(255) NOT NULL,
